c8d23c5bd5ab70d0980b5a2855942833cad2759c
John <128350391+alpaOo@users.noreply.github.com>
2023-09-28 15:36:39 +0800

    Update config files and respective files for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = "style-loader";
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
 
 const config = {
   entry: "./src/index.js",
@@ -21,8 +24,9 @@ const config = {
     host: "localhost",
   },
   plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // new MiniCssExtractPlugin({
+    //   filename: 'style.css',
+    // })
   ],
   module: {
     rules: [
@@ -44,6 +48,8 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
   } else {
     config.mode = "development";
   }
